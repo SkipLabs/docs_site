e01374882cdc09d23162f510a46faad0c88b514a
Hugo Venturini <hugo@skiplabs.io>
2026-05-26 18:55:38 +0200

    update to 1608dfb92

---
+++ assets/js/acecf23e.7c90e12d.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwww=globalThis.webpackChunkwww||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Skip Blog","authorsListPath":"/blog/authors"}')}}]);
